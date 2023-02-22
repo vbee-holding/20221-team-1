7e947025d97844a50a65fce71a61bd83e16fc183
NguyenDacHoangAnh <anh.ndh204510@sis.hust.edu.vn>
2023-02-22 11:01:43 +0700

    Hien thi danh sach tat ca tran dau

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
-import Navbar from './Components/navbar.js'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/header/Header';
+import AllMatches from './scenes/AllMatches/AllMatches';
+// import { AllPost, PostAboutTeam } from './scenes/AllPosts/Post';
+// import DetailMatch from './scenes/DetailMatch/DetailMatch';
+// import DetailTeam from './scenes/DetailTeam/DetailTeam';
+// import MatchBracket from './scenes/MatchBracket/MatchBracket';
+// import PlayerByTeam from './scenes/PlayerByTeam/PlayerByTeam';
+// import RankByTeam from './scenes/RankByTeam/RankByTeam';
+// import Ranking from './scenes/Ranking/Ranking';
+// import Statistic from './scenes/Statistic/Statistic';
+// import TopPlayer from './scenes/TopPlayer/TopPlayer'
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
       <Router>
-        <Navbar />
+        <Header />
+        <Routes>
+          <Route path='/' element={<AllMatches />}></Route>
+          {/* <Route path='/matches/:id' element={<DetailMatch />}></Route> */}
+          {/* <Route path='/topPlayer' element={<TopPlayer />}></Route> */}
+          {/* <Route path='/statistic' element={<Statistic />}></Route> */}
+          {/* <Route path='/posts' element={<AllPost />}></Route> */}
+          {/* <Route path='/team/:id' element={<DetailTeam />}></Route> */}
+          {/* <Route path='/team/:idTeam/post' element={<PostAboutTeam />}></Route> */}
+          {/* <Route path='/team/:idTeam/players' element={<PlayerByTeam />}></Route> */}
+          {/* <Route path='/team/:idTeam/rank' element={<RankByTeam />}></Route> */}
+          {/* <Route path='/bracket' element={<MatchBracket />}></Route> */}
+          {/* <Route path='/rank' element={<Ranking />}></Route> */}
+        </Routes>
       </Router>
-
     </div>
   );
 }
