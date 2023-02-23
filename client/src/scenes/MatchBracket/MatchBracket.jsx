fe2c58d6f85c097723e8bdbe53d8801c05019f76
anh-nnd183475 <anh.nnd183475@sis.hust.edu.vn>
2023-02-23 16:44:51 +0700

    View matches bracket & Fix match container

--- client/src/scenes/MatchBracket/MatchBracket.jsx
+++ client/src/scenes/MatchBracket/MatchBracket.jsx
@@ -3,6 +3,10 @@ import './MatchBracket.css';
 
 const MatchBracket = () => {
     return (
+        <div>
+        <div class="bracket-head">
+        <h3>SƠ ĐỒ THI ĐẤU VÒNG LOẠI TRỰC TIẾP</h3>
+        </div>
         <div className="match-bracket">
             <div class="row">
                 <div class="column">
@@ -12,19 +16,19 @@ const MatchBracket = () => {
                                 <div class="column">
                                     <div class="row">
                                         <div class="column">
-                                            <div><Match idMatch={53} isBracket={true} /></div>
-                                            <div class="border"></div>
-                                            <div><Match idMatch={54} isBracket={true} /></div>
+                                            <div><Match idMatch={49} isBracket={true} /></div>
+                                            
+                                            <div><Match idMatch={50} isBracket={true} /></div>
                                         </div>
-                                        <div><Match idMatch={57} isBracket={true} /></div>
+                                        <div><Match idMatch={58} isBracket={true} /></div>
                                     </div>
 
                                     <div class="row">
                                         <div class="column">
-                                            <div><Match idMatch={49} isBracket={true} /></div>
-                                            <div><Match idMatch={50} isBracket={true} /></div>
+                                            <div><Match idMatch={53} isBracket={true} /></div>
+                                            <div><Match idMatch={54} isBracket={true} /></div>
                                         </div>
-                                        <div><Match idMatch={58} isBracket={true} /></div>
+                                        <div><Match idMatch={57} isBracket={true} /></div>
                                     </div>
                                 </div>
                                 <div><Match idMatch={61} isBracket={true} /></div>
@@ -51,12 +55,17 @@ const MatchBracket = () => {
                                 <div><Match idMatch={62} isBracket={true} /></div>
                             </div>
                         </div>
-                        <div><Match idMatch={64} isBracket={true} /></div>
+                        <div class="column">
+                            <div><Match idMatch={64} isBracket={true} /></div>
+                            <div class="distance"></div>
+                            <div><Match idMatch={63} isBracket={true}/></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
+        <div class="distance"></div>
+    </div>
     )
 }
 
